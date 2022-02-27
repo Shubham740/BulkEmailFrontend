8fe286c42bd19d60886023cd98450599a0b21bcc
shubham740 <shubhampusa740@gmail.com>
2022-02-27 08:03:55 +0530

    merged redux code

--- src/screens/dashboard/Dashboard.js
+++ src/screens/dashboard/Dashboard.js
@@ -14,6 +14,7 @@ import { useSelector } from 'react-redux'
 const Dashboard = () => {
   const history = useHistory();
 
+  
   const [isNavigatorOpen, setIsNavigatorOpen] = useState(false)
 
   const emailId = useSelector(state => state.LoginReducer.email)
